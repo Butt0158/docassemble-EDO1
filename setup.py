472bb326b2dcef42c6ed3d1254afe287bdc7eca8
Jordan Buttery <101305713+Butt0158@users.noreply.github.com>
2022-05-02 14:39:53 +0930

    v5

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.EDO1',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.EDO1\n\nA docassemble extension.\n\n## Author\n\nJordan Buttery, butt0158@flinders.edu.au\n\n',
+      long_description='# docassemble.EDO1\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nJordan Buttery, butt0158@flinders.edu.au\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Jordan Buttery',
       author_email='butt0158@flinders.edu.au',
